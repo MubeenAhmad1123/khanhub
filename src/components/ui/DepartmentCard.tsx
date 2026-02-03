63210f78a0a2f00240da5e37e6f100639d73dd4c
MubeenAhmad1123 <mubeenahma1123@gmail.com>
2026-02-03 15:30:13 +0500

    department card

--- src/components/ui/DepartmentCard.tsx
+++ src/components/ui/DepartmentCard.tsx
@@ -127,7 +127,7 @@ export default function DepartmentCard({ department, index = 0 }: DepartmentCard
                                         <svg className="w-3 h-3 mr-1.5 text-primary-500" fill="currentColor" viewBox="0 0 20 20">
                                             <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                         </svg>
-                                        {service}
+                                        {typeof service === 'string' ? service : service.title}
                                     </motion.span>
                                 ))}
                                 {department.services.length > 3 && (
