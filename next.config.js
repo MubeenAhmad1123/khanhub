d2a25be9690822f6a7b65495bb97d1afea231e65
MubeenAhmad1123 <mubeenahma1123@gmail.com>
2026-03-20 19:24:06 +0500

    add more secrutiy try ti increse it from d

--- next.config.js
+++ next.config.js
@@ -39,6 +39,10 @@ const nextConfig = {
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googleapis.com *.cloudinary.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src * data: blob:; media-src * blob:; connect-src *; font-src 'self' fonts.gstatic.com;"
+          },
         ],
       },
     ]
